5b07ad3abd987080dd571f1912250ca6a53a7272
selami538 <erdaltayfun37@gmail.com>
2025-11-05 13:30:45 +0300

    Update [path].js

--- functions/hls/[path].js
+++ functions/hls/[path].js
@@ -1,17 +1,18 @@
 export async function onRequest({ request }) {
   const parsedUrl = new URL(request.url);
 
-  // Cloudflare Pages domain’inde "/hls" altındaki isteği kırpıyoruz
+  // Artık /hls kısmını kaldırmıyoruz, çünkü origin’de de var
   const targetUrl = "https://esraerolseksi.global.ssl.fastly.net" +
-    parsedUrl.pathname.replace("/hls", "") +
+    parsedUrl.pathname +
     parsedUrl.search;
 
   const response = await fetch(targetUrl, {
-    cf: {
-      cacheEverything: true,
-      cacheTtl: 360
-    }
+    cf: { cacheEverything: true, cacheTtl: 360 },
+  }).catch(err => {
+    return new Response("Fetch error: " + err.message, { status: 500 });
   });
+
+  if (!(response instanceof Response)) return response;
 
   const newHeaders = new Headers(response.headers);
   newHeaders.delete("set-cookie");
@@ -19,6 +20,6 @@ export async function onRequest({ request }) {
 
   return new Response(response.body, {
     status: response.status,
-    headers: newHeaders
+    headers: newHeaders,
   });
 }
