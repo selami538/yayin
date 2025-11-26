71fbeb6cadd47452c4576d459e35eb4ec392da4a
selami538 <erdaltayfun37@gmail.com>
2025-11-26 17:57:41 +0300

    Update [path].js

--- functions/hls/[path].js
+++ functions/hls/[path].js
@@ -1,24 +1,26 @@
-export async function onRequest({ request }) {
+export async function onRequest(context) {
+  const request = context.request;
   const parsedUrl = new URL(request.url);
 
-  // Artık /hls kısmını kaldırmıyoruz, çünkü origin’de de var
-  const targetUrl = "https://esraerolseksi.global.ssl.fastly.net" +
+  const targetUrl =
+    "https://esraerolseksi.global.ssl.fastly.net" +
     parsedUrl.pathname +
     parsedUrl.search;
 
-  const response = await fetch(targetUrl, {
-    cf: { cacheEverything: true, cacheTtl: 360 },
-  }).catch(err => {
+  let response;
+  try {
+    response = await fetch(targetUrl);
+  } catch (err) {
     return new Response("Fetch error: " + err.message, { status: 500 });
-  });
+  }
 
-  if (!(response instanceof Response)) return response;
+  const data = await response.arrayBuffer();
 
   const newHeaders = new Headers(response.headers);
   newHeaders.delete("set-cookie");
   newHeaders.set("Access-Control-Allow-Origin", "*");
 
-  return new Response(response.body, {
+  return new Response(data, {
     status: response.status,
     headers: newHeaders,
   });
